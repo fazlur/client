05a592450d02bda8ff6d30c553b017edca2d95d7
Fazlur Abba <fazlur.abba@gmail.com>
2021-03-21 21:44:23 +0000

    Adding Label and field for Name and Surname Adding Panel to Frame

--- app/src/main/java/client/app/ClientApp.java
+++ app/src/main/java/client/app/ClientApp.java
@@ -11,29 +11,43 @@ public class ClientApp {
     public static void main(String[] args) {
         // Troca de mensagem (uso de objetos)
         JFrame window = new JFrame("Client Application");
-        JPanel thePanel = new JPanel();
-        Client model = new DefaultClient("Edson", "Prestes"); // Composição
-        ClientRepository repository = null;
+
+
+        JPanel thePanel = new JPanel(null);//Set to null to allow positioning of components
 
         JLabel nameLabel = new JLabel("Name");
         nameLabel.setBounds(65, 31, 46, 14);
         thePanel.add(nameLabel);
 
         JTextField nameField = new JTextField();
-        nameField.setBounds(128, 28, 86, 20);
+        nameField.setBounds(128, 31, 86, 20);
         nameField.setColumns(10);
         thePanel.add(nameField);
 
+        JLabel surnameLabel = new JLabel("Surname");
+        surnameLabel.setBounds(65, 61, 56, 14);
+        thePanel.add(surnameLabel);
+
+        JTextField surnameField = new JTextField();
+        surnameField.setBounds(128, 61, 86, 20);
+        surnameField.setColumns(10);
+        thePanel.add(surnameField);
+
+
+        Client model = new DefaultClient(nameField.getText(), surnameField.getText()); // Composição
+        ClientRepository repository = null;
 
       /*  model.addEMail(JOptionPane.showInputDialog("Please, type in your email."));
         model.addCellPhone(JOptionPane.showInputDialog("Mobile Number"));
         System.out.println(model);*/
 
+
+        window.add(thePanel);
+
         window.setSize(640, 480);
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
-        window.add(thePanel);
 
     }
 }
