98591d91c20a3314ba61f00139837a1b59fd13db
Fazlur Abba <fazlur.abba@gmail.com>
2021-03-21 16:41:56 +0000

    playing with Frame mainly for 1st commit / push test

--- app/src/main/java/client/app/ClientApp.java
+++ app/src/main/java/client/app/ClientApp.java
@@ -11,16 +11,29 @@ public class ClientApp {
     public static void main(String[] args) {
         // Troca de mensagem (uso de objetos)
         JFrame window = new JFrame("Client Application");
+        JPanel thePanel = new JPanel();
         Client model = new DefaultClient("Edson", "Prestes"); // Composição
         ClientRepository repository = null;
 
-        model.addEMail(JOptionPane.showInputDialog("Please, inform your email."));
-        model.addCellPhone("012359841");
-        System.out.println(model);
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setBounds(65, 31, 46, 14);
+        thePanel.add(nameLabel);
+
+        JTextField nameField = new JTextField();
+        nameField.setBounds(128, 28, 86, 20);
+        nameField.setColumns(10);
+        thePanel.add(nameField);
+
+
+      /*  model.addEMail(JOptionPane.showInputDialog("Please, type in your email."));
+        model.addCellPhone(JOptionPane.showInputDialog("Mobile Number"));
+        System.out.println(model);*/
 
         window.setSize(640, 480);
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        // window.setVisible(true);
+        window.setVisible(true);
+        window.add(thePanel);
+
     }
 }
