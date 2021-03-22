5feae911ee52bf38bd60a97031dc9382bb012e58
Fazlur Abba <fazlur.abba@gmail.com>
2021-03-22 21:55:04 +0000

    Completing a bit more of the frame

--- app/src/main/java/client/app/ClientApp.java
+++ app/src/main/java/client/app/ClientApp.java
@@ -6,13 +6,12 @@ import client.repository.ClientRepository;
 
 import javax.swing.*;
 
-public class ClientApp {
+public class ClientApp  {
 
     public static void main(String[] args) {
         // Troca de mensagem (uso de objetos)
         JFrame window = new JFrame("Client Application");
-
-
+        JButton submitButton = new JButton("Submit");
         JPanel thePanel = new JPanel(null);//Set to null to allow positioning of components
 
         JLabel nameLabel = new JLabel("Name");
@@ -33,14 +32,43 @@ public class ClientApp {
         surnameField.setColumns(10);
         thePanel.add(surnameField);
 
+        JLabel cellPhoneLabel = new JLabel("CellPhone");
+        cellPhoneLabel.setBounds(65, 91, 86, 20);
+        thePanel.add(cellPhoneLabel);
 
-        Client model = new DefaultClient(nameField.getText(), surnameField.getText()); // Composição
-        ClientRepository repository = null;
+        JTextField cellPhoneField = new JTextField();
+        cellPhoneField.setBounds(128, 91, 86, 20);
+        cellPhoneField.setColumns(11);
+        thePanel.add(cellPhoneField);
 
-      /*  model.addEMail(JOptionPane.showInputDialog("Please, type in your email."));
+        JLabel emailLabel = new JLabel("Email");
+        emailLabel.setBounds(65, 121, 86, 20);
+        thePanel.add(emailLabel);
+
+        JTextField emailField = new JTextField();
+        emailField.setBounds(128, 121, 186, 20);
+        emailField.setColumns(50);
+        thePanel.add(emailField);
+
+        /*  model.addEMail(JOptionPane.showInputDialog("Please, type in your email."));
         model.addCellPhone(JOptionPane.showInputDialog("Mobile Number"));
         System.out.println(model);*/
 
+        submitButton.setBounds(158, 300, 86, 20);
+        thePanel.add(submitButton);
+
+  /*      submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });*/
+
+        Client model = new DefaultClient(nameField.getText(), surnameField.getText()); // Composição
+        ClientRepository repository = null;
+        model.addEMail(emailField.getText());
+        model.addCellPhone(cellPhoneField.getText());
+        //System.out.println(model);
 
         window.add(thePanel);
 
@@ -49,5 +77,8 @@ public class ClientApp {
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
 
+
     }
+
+
 }
